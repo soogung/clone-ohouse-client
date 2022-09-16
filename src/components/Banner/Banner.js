0c04894e752a0cdd89da4c16a47fcd2ca886757d
sookyoungwoo <sookyoung2005@gmail.com>
2022-09-16 22:11:55 +0900

    UPDATE | 전체적인 수정

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,40 @@
+import './Banner.scss'
+import {Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Pagination, Autoplay } from "swiper";
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import {useState} from "react";
+import {Link} from "react-router-dom";
+
+SwiperCore.use([ Pagination, Autoplay])
+
+function Banner(props){
+    const[activeIndex, setActiveIndex] = useState(1)
+    return(
+        <div className='banner_tit'>
+            <div className='banner-swiper'>
+                <Swiper
+                    className='banner'
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    pagination={{clickable: true}}
+                    autoplay={{delay: 3000}}
+                    watchOverflow={true}
+                >
+                    {props.banner.map(b => (
+                        <SwiperSlide>
+                            <Link to ="">
+                                <img className='banner--image' src={b.img} alt="banner"/>
+
+                            </Link>
+                        </SwiperSlide>
+                    ))
+                    }
+                </Swiper>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,14 @@
+import './Main.scss';
+import Nav from '../../components/Nav/Nav';
+import BannerData from './banner.json';
+import Banner from '../../components/Banner/Banner';
+export default function Main(){
+    return(
+        <section className='Main-section'>
+            <Nav />
+            <Banner
+                banner={BannerData.banners}
+            />
+        </section>
+    )
+}
