3fdb072c1cc83eb4d8b43e90a2a5af737cdac750
sjy1410 <chicken010208@gmail.com>
2022-10-11 08:32:30 +0900

    로그인

---
+++ src/components/Signup_head/Signup_head.js
@@ -0,0 +1,9 @@
+import "./Signup_head.scss";
+
+export default function Signup_head() {
+  return (
+    <>
+      <h1>asf</h1>
+    </>
+  );
+}
---
+++ src/components/Signup_main/Signup_main.js
@@ -0,0 +1,9 @@
+import "./Signup_main.scss";
+
+export default function Signup_main() {
+  return (
+    <>
+      <h1>asf</h1>
+    </>
+  );
+}
