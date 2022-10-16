872b7d35de19cbec67f64c47e331b06d380eb5ed
sookyoungwoo <sookyoung2005@gmail.com>
2022-10-16 22:38:20 +0900

    ERROR :: 메인페이지 상품 나열 완료했으나 화면 크기 오류

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -7,14 +7,14 @@ import MainCate from '../../components/MainCate/MainCate';
 import SubCateData from '../../components/Nav/category.json';
 import SubCate from '../../components/SubCate/SubCate';
 import { SwiperSlide, Swiper } from "swiper/react";
-import SwiperCore, {Navigation} from 'swiper';
+import SwiperCore, { Navigation } from 'swiper';
 import "swiper/scss";
 import Footer from '../../components/Footer/Footer';
 import ProductArticle from '../../components/ProductArticle/ProductArticle';
 import ProductData from './productarticle.json';
-export default function Main(){
+export default function Main() {
     SwiperCore.use([Navigation])
-    return(
+    return (
         <section className='Main-section'>
             <div className='main-nav'>
                 <Nav />
@@ -36,18 +36,20 @@ export default function Main(){
                 <div className='product-article-total'>
                     <header className='productarticle-header'>
                         <h1>오늘의 딜</h1>
-                       <a>더보기</a>
+                        <a>더보기</a>
                     </header>
                 </div>
-                <div className='main-proudct-today-deal'>
+            </div>
+            <section className='productarticle-section'>
+                <div className='article-tit'>
                     {
                         ProductData.products.map(p => (
                             <ProductArticle
                                 id={p.id}
                                 name={p.name}
                                 isDiscount={p.isDiscout}
-                                discountDegree = {p.discountDegree}
-                                mainImage = {p.mainImage}
+                                discountDegree={p.discountDegree}
+                                mainImage={p.mainImage}
                                 price={p.price}
                                 discountedPrice={p.discountedPrice}
                                 company={p.company}
@@ -55,25 +57,25 @@ export default function Main(){
                         ))
                     }
                 </div>
-            </div>
+            </section>
             <div className='sub-category-subcate'>
                 <a className='sub-category-categorytit'>카테고리</a>
                 <Swiper
                     spaceBetween={10}
                     slidesPerView={10}
                     slidesPerGroup={10}
-                    navigation    
+                    navigation
                 >
-                {
-                    SubCateData.category.map(ca => (
-                        <SwiperSlide>
-                            <SubCate
-                                image={ca.img}
-                                name={ca.name}
-                            />
-                        </SwiperSlide>
-                    ))
-                }
+                    {
+                        SubCateData.category.map(ca => (
+                            <SwiperSlide>
+                                <SubCate
+                                    image={ca.img}
+                                    name={ca.name}
+                                />
+                            </SwiperSlide>
+                        ))
+                    }
                 </Swiper>
             </div>
             <div></div>
