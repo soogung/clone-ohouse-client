ca6bc8e9089692a58e603cacb4194bd68b66c99b
gimhanul <gimhanuly@gmail.com>
2022-10-27 08:01:15 +0900

    ADD :: product detail images (#9)
    - product detail images 추가
    - 선택된 image 표시

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,20 +1,37 @@
 import DetailPage from "./ProductDetailPage";
 import DetailData from "./productDetail.json";
 import './ProductDetailPage.scss';
+import {useEffect, useState} from "react";
+import {customAxios} from "../../config/axiosConfig";
+import {useParams} from "react-router-dom";
+
 export default function ProductDetail() {
+    const {id} = useParams();
+    const [product, setProduct] = useState({})
+
+    const getData = async () => {
+        try {
+            const res = await customAxios.get(`/product/${id}`);
+            setProduct({...res.data})
+        } catch (error) {
+            alert(error.response.data.status + ": " + error.response.data.message);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
 
     return (
         <section className="detailpage-section">
             {
-                DetailData.data.map(d => (
-                    <DetailPage
-                        deliverytype={d.delivery}
-                        discountdegree={d.discountDegree}
-                        images={d.images}
-                        name={d.name}
-                        productoption={d.productOption}
-                    />
-                ))
+                <DetailPage
+                    deliverytype={product.delivery}
+                    discountdegree={product.discountDegree}
+                    images={product.images}
+                    name={product.name}
+                    productoption={product.productOption}
+                />
             }
         </section>
     )
